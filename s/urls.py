017ce489a8c01893115c64d7b27d9241e14dce4a
Tom Huibregtse <t.huibr@gmail.com>
2024-04-05 09:19:07 -0500

    black

--- s/urls.py
+++ s/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.http import HttpResponse
 from django.shortcuts import render
@@ -25,6 +26,6 @@ def hello(request):
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     path("", hello, name="hello"),
 ]
