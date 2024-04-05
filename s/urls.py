28729aef45b56bd6a8d55f95d1bddc34d26d7c8b
Tom Huibregtse <t.huibr@gmail.com>
2024-04-05 06:44:16 -0500

    add fly hello

--- s/urls.py
+++ s/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
+
+
+def hello(request):
+    return HttpResponse("Hello, Fly!")
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", hello, name="hello"),
 ]
